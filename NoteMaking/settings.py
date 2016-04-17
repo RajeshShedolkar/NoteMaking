0e6f8a8233c1eb6f11496c59e54ca8ed7f1518ae
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-17 14:17:48 +0530

    added path for static folder

--- NoteMaking/settings.py
+++ NoteMaking/settings.py
@@ -15,6 +15,7 @@ import os
 
 BASE_DIR =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+HOSTNAME_URL = 'http://127.0.0.1:8000'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -100,3 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
