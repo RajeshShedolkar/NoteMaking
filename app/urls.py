8ead7a2c9bf68a999acf67fa9e719eaed60dcb2b
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-17 14:18:42 +0530

    adding /dashboard url

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
     # url(r'^confirm/(?P<activation_key>\w+)/$',views.confirm,name='vendor_login'),
     # url(r'^ajax$', views.ajax, name='ajax'),
     # url(r'^search/washing-machine$', views.washing_machine, name='washing_machine'),
