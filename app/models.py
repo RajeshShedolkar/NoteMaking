1f1c233671e39c501753178106420103db0fff12
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-19 17:19:58 +0530

    adding user_details model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class user_details(models.Model):
+    full_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __unicode__(self):
+        return self.username
