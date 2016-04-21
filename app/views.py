5ee04c52a711afa9ce4840f8c049542ee8055d03
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-21 17:36:19 +0530

    storing and sending data to the dashaboard

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http40
 from app.models import *
 from django.contrib import messages
 import uuid
+import datetime
 # Create your views here.
 def index(request):
     print "hello world"
@@ -17,13 +18,18 @@ def login(request):
         print request.POST
         username = request.POST['username']
         firstname = request.POST['firstname']
+        password = request.POST['password']
         age = request.POST['age']
+        test = request.POST['test']
         try :
             age = int(age)
         except:
             messages.error(request, "age shuld be number")
             return render(request,'login.html',{"t":"rajesh shedolkar"})
-        u = user_details(full_name=firstname, username=username, age=age)
+        user = User.objects.create_user(username=username, password=password, first_name=firstname, email=username)
+        user.save()
+
+        u = user_details(user=user, full_name=firstname, age=age, test=test)
         u.save()
         return HttpResponseRedirect('/dashboard')
 
@@ -42,11 +48,12 @@ def login(request):
 
 def dashboard(request):
     t = user_details.objects.all() #getting all data from te table
+    user_data = User.objects.all()
     d = []
     notes = []
     data = {'data':d,'notes':notes}
-    for i in t:
-        di = {'username':i.username, 'full_name':i.full_name, 'age':i.age}
+    for i in range(len(t)):
+        di = {'username':user_data[i].username, 'full_name':user_data[i].first_name, 'age':t[i].age, 'test':t[i].test}
         d.append(di)
     if request.method=="POST":
         print request.POST
@@ -55,6 +62,7 @@ def dashboard(request):
         if note:
             u = NoteMaking(notes_id=notes_id, note=note)
             u.save()
+            print notes_id
         else:
             messages.error(request, "it's empty")
             return HttpResponseRedirect('/dashboard')
