690fb6ad2d36d436cbf8b98f64d021eb16c0dab4
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-21 17:35:04 +0530

    changing user_details model and using 'User' auth user model

--- app/models.py
+++ app/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class user_details(models.Model):
+    user = models.OneToOneField(User)
+    #user has ['username','password','full_name','email']
     full_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
+    #username = models.CharField(max_length=50)
     age = models.IntegerField()
-
+    test = models.CharField(max_length=50)
     def __unicode__(self):
-        return self.username
+        return self.full_name
 
 # class Notes(models.Model):
 #     user_id = CharField(max_length=50)
