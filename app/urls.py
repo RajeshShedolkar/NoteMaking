00b720bb71062e7c58d777cf3ee56a4dddcbc512
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-28 10:43:26 +0530

    added 'app' and 'api' respectives urls

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,17 @@
-from django.conf.urls import url, include
-
+from django.conf.urls import url, include,patterns
 from . import views
 
 #urls which send POST/GET data must not end with a slash
 
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+urlpatterns = patterns( 'app.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^login/$', 'login', name='login'),
+    url(r'^dashboard/$', 'dashboard', name='dashboard'),
+    url(r'^register/$', 'register', name='register'),
     # url(r'^confirm/(?P<activation_key>\w+)/$',views.confirm,name='vendor_login'),
     # url(r'^ajax$', views.ajax, name='ajax'),
     # url(r'^search/washing-machine$', views.washing_machine, name='washing_machine'),
-]
+)
+# urlpatterns = patterns( 'api.views',
+#     url(r'^abc/$', 'ind', name='ind'),
+# )
